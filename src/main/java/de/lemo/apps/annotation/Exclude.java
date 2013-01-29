2323ffea87e94fbd38a24815875aac4480827c24
Boris Wenzlaff <boris.wenzlaff@htw-berlin.de>
2013-01-29 15:40:09 +0100

    TEST Committ

--- src/main/java/de/lemo/apps/annotation/Exclude.java
+++ src/main/java/de/lemo/apps/annotation/Exclude.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Exclude {
 
+	//Das ist eine TESTÄNDERUNG kann gelöscht werden
 public String mode() default "ALL";
 public String[] stylesheet();
 }
