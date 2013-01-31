482045a0febb92675ffc5e71d8283ac0c7991660
Leonard Kappe <kappe@htw-berlin.de>
2013-01-31 12:48:32 +0100

    Bar chart CSV export

--- src/main/resources/de/lemo/apps/js/d3/nvd3_custom_LO_Bar_Chart.js
+++ src/main/resources/de/lemo/apps/js/d3/nvd3_custom_LO_Bar_Chart.js
@@ -1,64 +1,79 @@
 (function(d3custom, $, undefined) {
 
+  function csvExport(data) {
+    
+    var headers = ["Course", data[0].key, data[1].key];
+    var course = function(data, row) {
+      return data[0].values[row].x;
+    };
+    var request = function(data, row) {
+      return data[0].values[row].y;
+    };
+    var user = function(data, row) {
+      return data[1].values[row].y;
+    };
+
+    // TODO i18n
+    var exportButton = $('<div class="button export-button">Export to CSV</div>');
+    $("#viz").append(exportButton);
+
+    exportButton.click(function() {
+      var csvData = dataExport.createCSV(data, data[0].values.length, [course, request, user], headers);
+      console.log(csvData);
+    });
+
+  }
 
   d3custom.run = function() {
-	
-	  var data = [];
-	  
-	  data = d3custom.data;
-	  
-	  if(!data) {
-	    	$("#viz").prepend($('<div class="alert">No matching data found. Please check your filter setting.</div>'));
-	    	return;
-	  }
-	  
-	  
-	  
-	  
-	  var  	dataRequests = data[0].values;
-	  		dataUser = data[1].values;
-	  
-	  console.log("Before Sort:"+dataRequests);
-	  
-	  dataRequests.sort(function sortfunction(a, b){return (b.y - a.y)});
-	  dataUser.sort(function sortfunction(a, b){return (b.y - a.y)});
-	  
-	  console.log("After Sort:"+dataRequests);
-	  
-	  data[0].values = dataRequests;
-	  
-	  function sortfunction(a, b){
-		  return (a.y - b.y) //causes the array to be sorted numerically and ascending
-	  }
-	  
-	  nv.addGraph(function() {
-		  var chart = nv.models.multiBarChart()
-		  	.showControls(false)
-		  	.reduceXTicks(false);
 
-		  chart.xAxis
-		           .tickFormat(function(d) {
-		              return d;
-		            });
+    var data = d3custom.data;
 
-		  chart.yAxis
-		  		.tickFormat(d3.format(''));
-		  
-		  d3.select('#viz svg')
-		      .datum(data)
-		      .transition().duration(500)
-		      .call(chart);
- 
-		  nv.utils.windowResize(chart.update); 
-		 
-		  d3.selectAll('.nv-x text')
-		    .attr('transform', 'translate(0,5)rotate(45)')
-		    .style('text-anchor', 'start'); // in later nvd3 versions using attr instead of style should work too
+    if (!data) {
+      $("#viz").prepend($('<div class="alert">No matching data found. Please check your filter setting.</div>'));
+      return;
+    }
 
-		  return chart;
-		});
-	  
-	  
+    var dataRequests = data[0].values;
+    dataUser = data[1].values;
+
+    console.log("Before Sort:" + dataRequests);
+
+    dataRequests.sort(function sortfunction(a, b) {
+      return (b.y - a.y);
+    });
+    dataUser.sort(function sortfunction(a, b) {
+      return (b.y - a.y);
+    });
+
+    console.log("After Sort:" + dataRequests);
+
+    data[0].values = dataRequests;
+    /**
+     * causes the array to be sorted numerically and ascending
+     */
+    function sortfunction(a, b) {
+      return (a.y - b.y);
+    }
+
+    nv.addGraph(function() {
+      var chart = nv.models.multiBarChart().showControls(false).reduceXTicks(false);
+
+      chart.xAxis.tickFormat(function(d) {
+        return d;
+      });
+
+      chart.yAxis.tickFormat(d3.format(''));
+
+      d3.select('#viz svg').datum(data).transition().duration(500).call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      d3.selectAll('.nv-x text').attr('transform', 'translate(0,5)rotate(45)').style('text-anchor', 'start');
+
+      return chart;
+    });
+
+
   };
 
 })(window.d3custom = window.d3custom || {}, jQuery);
