67dc192b7c97e7272188e7f461fb0a466b66a83e
Leonard Kappe <kappe@htw-berlin.de>
2013-02-22 15:28:19 +0100

    Documentation

--- src/main/java/de/lemo/apps/entities/Roles.java
+++ src/main/java/de/lemo/apps/entities/Roles.java
@@ -5,11 +5,24 @@
  */
 package de.lemo.apps.entities;
 
-
+/**
+ * Roles for users of the application server.
+ */
 public enum Roles {
 
+	/**
+	 * Administrator role with access to user management but no access to LMS content.
+	 */
 	ADMIN,
+
+	/**
+	 * Global user with unrestricted access to all courses.
+	 */
 	GLOBAL,
+
+	/**
+	 * Default role with access to specified courses.
+	 */
 	TEACHER,
 
 }
