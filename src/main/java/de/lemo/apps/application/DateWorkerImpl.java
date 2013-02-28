12f82bf4dc1f1d07fccbc82adf9b0b1c22843143
Leonard Kappe <kappe@htw-berlin.de>
2013-02-28 19:35:46 +0100

    Use default asset location, removes duplicated asset

--- src/main/java/de/lemo/apps/application/DateWorkerImpl.java
+++ src/main/java/de/lemo/apps/application/DateWorkerImpl.java
@@ -1,19 +1,18 @@
 /**
-	 * File DateWorkerImpl.java
-	 *
-	 * Date Feb 14, 2013 
-	 *
-	 */
+ * File DateWorkerImpl.java
+ * Date Feb 14, 2013
+ */
 package de.lemo.apps.application;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import org.apache.tapestry5.Asset;
+import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.ioc.Messages;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.json.JSONLiteral;
-import org.apache.tapestry5.services.AssetSource;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.slf4j.Logger;
@@ -22,24 +21,23 @@ public class DateWorkerImpl implements DateWorker {
 
 	@Inject
 	private Logger logger;
-	
+
 	@Inject
 	private Messages messages;
-
-	private final AssetSource assetSource;
 
 	/**
 	 * Defining all constants used within this class
 	 */
 	public static final String DATE_FORMAT = "MMM dd, yyyy";
-	public static final String CALENDAR_ICON_PATH = "layout/images/glyphicons_045_calendar.png";
 	public static final int MILLISEC_MULTIPLIER = 1000;
 
-	public DateWorkerImpl(final Logger logger, final AssetSource assetSource) {
-		this.assetSource = assetSource;
-	}
+	@Inject
+	@Path("de/lemo/apps/images/icons/glyphicons_045_calendar.png")
+	private Asset calendarIcon;
 
 	public int daysBetween(final Calendar startDate, final Calendar endDate) {
+		// TODO a loop seems inefficient for this. Maybe use joda time, it has a `daysBetween` method.
+		// Is the the result correct? - We get '1 day' even if the dates are only seconds apart.
 		final Calendar date = (Calendar) startDate.clone();
 		int daysBetween = 0;
 		while (date.before(endDate)) {
@@ -50,7 +48,7 @@ public class DateWorkerImpl implements DateWorker {
 	}
 
 	public int daysBetween(final Date startDate, final Date endDate) {
-
+		// TODO see daysBetween(Calendar, Calendar)
 		final Calendar startDateTemp = Calendar.getInstance();
 		final Calendar endDateTemp = (Calendar) startDateTemp.clone();
 		endDateTemp.setTime(endDate);
@@ -86,10 +84,6 @@ public class DateWorkerImpl implements DateWorker {
 		return dfDate.format(date);
 	}
 
-	public String getCalendarIconPath() {
-		return this.assetSource.getContextAsset(DateWorkerImpl.CALENDAR_ICON_PATH, null).toString();
-	}
-
 	public JSONLiteral getDatePickerParams(final Locale locale) {
 		String monthNamesShort = "";
 		try {
@@ -97,7 +91,7 @@ public class DateWorkerImpl implements DateWorker {
 			 * Force jquery ui datepicker to use short month names of the current Java version. Datepicker uses the
 			 * correct
 			 * new ones, though on the backend it's depending on the Java version. The short name for march in german
-			 * changed from 'Mrz' to 'M�r' in Java 8 for example. Don't mind the 13th month, datepicker ignores it
+			 * changed from 'Mrz' to 'Mär' in Java 8 for example. Don't mind the 13th month, datepicker ignores it
 			 * anyway.
 			 */
 			monthNamesShort = new JSONArray(new java.text.DateFormatSymbols(locale).getShortMonths()).toString();
@@ -112,7 +106,7 @@ public class DateWorkerImpl implements DateWorker {
 				", buttonText: '" + this.messages.get("chooseDate") + "'" +
 				// ", minDate: '01.07.2012'" +
 				// ", maxDate: '"+this.course.getLastRequestDate()+
-				", buttonImage: '" + this.getCalendarIconPath() + "'" +
+				", buttonImage: '" + this.calendarIcon.toClientURL() + "'" +
 				", buttonImageOnly: false" +
 				", showButtonPanel: true" +
 				", dateFormat: 'M dd, yy'" +
