d57f29e8d0619aa8d842aa8be662ea1cd8525d35
Kluesener <mkluesener@gmx.net>
2014-12-19 11:29:52 +0100

    Adjusted arrows to border of nodes.

--- src/main/resources/de/lemo/apps/js/d3/ActivityGraph2.js
+++ src/main/resources/de/lemo/apps/js/d3/ActivityGraph2.js
@@ -12,7 +12,7 @@
 		vis,
 		root;
 
-		
+
 		$(window).resize(function() {
 			width = $('#viz').width();
 			d3.select("svg")
@@ -44,19 +44,19 @@
 			.attr('width', w)
 			.attr('height', h);
 			/* Build the directional arrows for the links/edges */
-	        svg.append("svg:defs")
-	                    .selectAll("marker")
-	                    .data(["end"])
-	                    .enter().append("svg:marker")
-	                    .attr("id", String)
-	                    .attr("viewBox", "0 -5 10 10")
-	                    .attr("refX", 15)
-	                    .attr("refY", -1.5)
-	                    .attr("markerWidth", 6)
-	                    .attr("markerHeight", 6)
-	                    .attr("orient", "auto")
-	                    .append("svg:path")
-	                    .attr("d", "M0,-5L10,0L0,5");
+			svg.append("svg:defs")
+			.selectAll("marker")
+			.data(["end"])
+			.enter().append("svg:marker")
+			.attr("id", String)
+			.attr("viewBox", "0 -5 10 10")
+			.attr("refX", 9)
+			.attr("refY", 0)
+			.attr("markerWidth", 6)
+			.attr("markerHeight", 6)
+			.attr("orient", "auto")
+			.append("svg:path")
+			.attr("d", "M0,-5L10,0L0,5");
 
 			var _nodes = d3custom.nodes,
 			_links = d3custom.links;
@@ -69,72 +69,80 @@
 
 			init();
 			function makeDiag() {
-			    /* Draw the node labels first */
-			   var texts = svg.selectAll("text")
-			                    .data(_nodes)
-			                    .enter()
-			                    .append("text")
-			                    .attr("fill", "black")
-			                    .attr("font-family", "sans-serif")
-			                    .attr("font-size", "10px")
-			                    .text(function(d) { return d.name; });
-			    /* Establish the dynamic force behavor of the nodes */
-			    var force = d3.layout.force()
-			                    .nodes(_nodes)
-			                    .links(_links)
-			                    .size([w,h])
-			                    .linkDistance([250])
-			                    
-			                   /* Add link distance depending on target node value.
-			                    * function(d) { 
+				/* Draw the node labels first */
+				var texts = svg.selectAll("text")
+				.data(_nodes)
+				.enter()
+				.append("text")
+				.attr("fill", "black")
+				.attr("font-family", "sans-serif")
+				.attr("font-size", "10px")
+				.text(function(d) { return d.name; });
+				/* Establish the dynamic force behavor of the nodes */
+				var force = d3.layout.force()
+				.nodes(_nodes)
+				.links(_links)
+				.size([w,h])
+				.linkDistance([250])
+
+				/* Add link distance depending on target node value.
+				 * function(d) { 
 			                    	var scale = d3.scale.linear()
                     				.domain([300, 900])
                     				.range([250, 50]);		
 			                    	console.log(d.target.value);
 			                    	return scale(d.target.value) })*/
-			                    .charge([-1500])
-			                    .gravity(0.3);
-			    /* Draw the edges/links between the nodes */
-			    var edges = svg.selectAll("line")
-			                    .data(_links)
-			                    .enter()
-			                    .append("line")
-			                    .style("stroke", "#ccc")
-			                    .style("stroke-width", 1)
-			                    .attr("marker-end", "url(#end)");
-			    /* Draw the nodes themselves */               
-			    var nodes = svg.selectAll("circle")
-			                    .data(_nodes)
-			                    .enter()
-			                    .append("circle")
-			                    .attr("r", 20)
-			                    .attr("opacity", 0.5)
-			                    .style("fill", function(d,i) { return color(d); })
-			                    .call(force.drag);
-			    	nodes.append("nodetitle")
-				     .text(function(d) { return "<b> "+locale.learningObject +":</b> "+ d.name+"<br /><br /><b> "+locale.resourcetypeLabel+" :</b> "+ d.type+"<br /><br /> <b> "+locale.activities+" </b>: "+d.value;});
-		        /* Run the Force effect */
-		        force.on("tick", function() {
-		                   edges.attr("x1", function(d) { return d.source.x; })
-		                        .attr("y1", function(d) { return d.source.y; })
-		                        .attr("x2", function(d) { return d.target.x; })
-		                        .attr("y2", function(d) { return d.target.y; });
-		                   nodes.attr("cx", function(d) { 	
-		    				if(d.connected==1){
-		    				return d.x;
-		    				} else return 30; })
-		                        .attr("cy", function(d) { return d.y; })
-		                   texts.attr("transform", function(d) {
-		    				if(d.connected==1){
-		    				return "translate(" + d.x + "," + d.y + ")";
-		    				} else return "translate(" + 30 + "," + d.y + ")";                        
-		                            });
-			                   }); // End tick func
-		        force.start();
-		        for (var i = 100; i > 0; --i) force.tick();
-		        force.stop();
+				.charge([-1500])
+				.gravity(0.3);
+				/* Draw the edges/links between the nodes */
+				var edges = svg.selectAll("line")
+				.data(_links)
+				.enter()
+				.append("line")
+				.style("stroke", "#ccc")
+				.style("stroke-width", 1)
+				.attr("marker-end", "url(#end)");
+				/* Draw the nodes themselves */               
+				var nodes = svg.selectAll("circle")
+				.data(_nodes)
+				.enter()
+				.append("circle")
+				.attr("r", 20)
+				.attr("opacity", 0.5)
+				.style("fill", function(d,i) { return color(d); })
+				.call(force.drag);
+				nodes.append("nodetitle")
+				.text(function(d) { return "<b> "+locale.learningObject +":</b> "+ d.name+"<br /><br /><b> "+locale.resourcetypeLabel+" :</b> "+ d.type+"<br /><br /> <b> "+locale.activities+" </b>: "+d.value;});
+				function normalize(d){
+					var deltaX = d.target.x - d.source.x,
+					deltaY = d.target.y - d.source.y,
+					dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+					normX = deltaX / dist,
+					normY = deltaY / dist;
+					return {normX,normY};
+				}				
+				/* Run the Force effect */
+				force.on("tick", function() {
+					edges.attr("x1", function(d) { return d.source.x + (20 * normalize(d).normX);})
+						 .attr("y1", function(d) { return d.source.y + (20 * normalize(d).normY);})
+						 .attr("x2", function(d) { return d.target.x - (20 * normalize(d).normX);})
+						 .attr("y2", function(d) { return d.target.y - (20 * normalize(d).normY);});
+					nodes.attr("cx", function(d) { 	
+						if(d.connected==1){
+							return d.x;
+						} else return 30; })
+						.attr("cy", function(d) { return d.y; })
+						texts.attr("transform", function(d) {
+							if(d.connected==1){
+								return "translate(" + d.x + "," + d.y + ")";
+							} else return "translate(" + 30 + "," + d.y + ")";                        
+						});
+				}); // End tick func
+				force.start();
+				for (var i = 100; i > 0; --i) force.tick();
+				force.stop();
 			}; // End makeDiag worker func
-			
+
 			function init(){
 				var support = d3custom.support/100;
 				var size = Math.floor(_links.length*(1-support));
@@ -153,13 +161,13 @@
 					_nodes[link.source].connected=1;
 				})			
 				makeDiag();
-			    // Bind slide event to update minSup Value in front end
-			    $("#sliderZone-slider").bind("slide", function(event, ui) {
-			      var oldLabel = $("#sliderZone-label").text().split(":");
-			      $("#sliderZone-label").html( oldLabel[0]+":" + ui.value / 100);
-			    });
+				// Bind slide event to update minSup Value in front end
+				$("#sliderZone-slider").bind("slide", function(event, ui) {
+					var oldLabel = $("#sliderZone-label").text().split(":");
+					$("#sliderZone-label").html( oldLabel[0]+":" + ui.value / 100);
+				});
 			}   
-			
+
 			function rescale() {
 				trans=d3.event.translate;
 				scale=d3.event.scale;
