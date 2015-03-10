9433a168ff43a52d4997c981f61d9cc424fa9b19
Kluesener <mkluesener@gmx.net>
2015-03-10 16:32:44 +0100

    Implemented datepicker for classification date range.

--- src/main/java/de/lemo/apps/components/ScatterPlotForm.java
+++ src/main/java/de/lemo/apps/components/ScatterPlotForm.java
@@ -1,9 +1,18 @@
 package de.lemo.apps.components;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.beaneditor.Validate;
+import org.apache.tapestry5.corelib.components.DateField;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONLiteral;
 
+import de.lemo.apps.application.DateWorker;
 import de.lemo.apps.entities.Courses;
 import de.lemo.apps.entities.Features;
 
@@ -27,4 +36,34 @@ public class ScatterPlotForm {
 	@Persist
 	@Validate("required")
 	private Courses current_course;
+	
+	@Inject
+	private DateWorker dateWorker;
+	
+	@Component(id = "beginDate")
+	private DateField beginDateField;
+
+	@Component(id = "endDate")
+	private DateField endDateField;
+	
+	@Property
+	@Persist
+	private Date beginDate;
+	
+	@Property
+	@Persist
+	private Date endDate;
+	
+	
+	public void setupRender(){
+		endDate = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(1381795200000L);
+		beginDate = calendar.getTime();		
+	}
+	// returns datepicker params
+	public JSONLiteral getDatePickerParams() {
+		JSONLiteral result = this.dateWorker.getDatePickerParams(new Locale("en"));
+		return result;
+	}
 }
